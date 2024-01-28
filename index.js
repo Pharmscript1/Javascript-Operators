2650d073bffde5666f83c7c892f2468f64edf168
Ekene Okpechukwu <86759472+Pharmscript1@users.noreply.github.com>
2024-01-28 11:35:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+//OPERATORS
+
+const a = 10;
+const b = 10;
+let result = 0; 
+
+//addition 
+// result = a + b;
+
+//subtraction
+//result = a - b;
+
+
+
+//Multiplication or division
+//result = a / b;
+ 
+//exponential
+//result = a ** b;
+
+// Module
+//result = a % b;
+
+//increment decrement
+//result--;
+//console.log(result);
+
+// Comparison operators ==> true/false
+ 
+//console.log(a >= b);
+//console.log( b > a);
+//console.log(a != b);
+
+//Strict vs loose equality
+
+console.log(5 == 5);
+console.log('String'== 'String');
+
+
+// Strict compares data types and values
+console.log(5 === "5");
+//Programmers stick to strict equality because it does not produce weirld results. 
+
+//loose does not compare data types
+console.log(5 == '5');
+
+//Logical Operators
+//AND && ==> All operands are true
+console.log(true && true && false);
+
+// OR || ==> at laeast one is true 
+console.log(false || false || true);
+
+// NOT ! 
+console.log(!true);
+
+// Assignment operator
+
+let number = 5;
+
+//number += 5;
+
+//number /= 5;
+
+let string = "Hello";
+string += ", I am Ekene!"
+
+console.log(string);
